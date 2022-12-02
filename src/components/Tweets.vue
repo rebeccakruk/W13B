2e4eb32e178deb15195aabe43a3c6b2f38db5c88
rk <rebeccakruk@outlook.com>
2022-12-02 08:30:27 -0700

    added data

--- src/components/Tweets.vue
+++ src/components/Tweets.vue
@@ -11,7 +11,7 @@ export default {
         return {
             tweets: ["Every clock in my room changed daylight time on its own. It is finally the future.", "I like to keep a corner of my classroom as a safe place for crying or tantrums. Now I just have to make a spot for the kids too.", "My toddler is pretending to cook a meal and it involves a lot of screaming and throwing food in a pot. She gets it."],
             creator: ["Kelly Oxford", "Kate Deal", "Caroline Bologna"],
-            dateCreated: ["2013-04-23", "2022-08-18", "2022-11-14"],
+            dateCreated: ["2013-04-23", "2022-08-18", "2022-11-14"]
         }
     },
 }
